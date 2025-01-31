d6659d36182ee0643f39cb7f76b0a6f707b2d90a
Bogdan <Bogdangulenko311@gmail.com>
2025-01-31 18:12:15 +0000

    Initial commit

---
+++ project4(cats in process of devolvoping).py
@@ -0,0 +1,19 @@
+def get_cats_info(path):
+    cats = []
+    try:
+         with open(path, 'r', encoding='utf-8') as file:
+              for line in file:
+                   parts = line.strip().split(',')
+                   if len(parts) == 3:
+                    cat = {
+                        "id": parts[0],
+                        "name": parts[1],
+                        "age": parts[2]
+                    }
+                    cats.append(cat)
+    except FileNotFoundError:
+       print(f"Файл за шляхом {path} не знайдено.")
+    except IOError:
+        print(f"Помилка при відкритті файлу {path}.")
+    
+    return cats                    
